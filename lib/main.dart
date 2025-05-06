5f7774aee6262de348ee835344dc0a0144f6e51c
Fisnik Duraku <fisnik.duraku@student.vinci.be>
2025-05-06 17:09:01 +0200

    add: automatic redirect user token in storage

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ import 'package:solid_cv/Views/UserPage.dart';
 import 'package:solid_cv/Views/VerifyACvRoute.dart';
 import 'package:solid_cv/Views/companyViews/AddAnEmployee.dart';
 import 'package:solid_cv/Views/educationInstitutionViews/CreateACertificate.dart';
+import 'package:solid_cv/Views/widgets/SessionValidatorToken.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,26 +32,30 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ).copyWith(
         appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.deepPurple,
-          foregroundColor: Colors.white,
-          shadowColor: Colors.black,
-          elevation: 5
-        ),
+            backgroundColor: Colors.deepPurple,
+            foregroundColor: Colors.white,
+            shadowColor: Colors.black,
+            elevation: 5),
       ),
+      home: const SessionValidatorTokenPage(),
       initialRoute: '/',
       routes: {
-        '/': (context) => const HomeRoute(),
+        '/login': (context) => const HomeRoute(),
         '/register': (context) => const RegisterRoute(),
         '/loggedin/home': (context) => const LoggedInHome(),
         '/my-cv': (context) => const MyCvRoute(),
         '/my-organisation': (context) => const MyOrganisationsRoute(),
-        '/my-educationInstitution-administration': (context) => MyEducationInstitutionAdministration(),
+        '/my-educationInstitution-administration': (context) =>
+            MyEducationInstitutionAdministration(),
         '/add-a-company-form': (context) => const AddACompanyFormRoute(),
-        '/add-a-education-institution-form': (context) => const AddanEducationInstitutionFormRoute(),
+        '/add-a-education-institution-form': (context) =>
+            const AddanEducationInstitutionFormRoute(),
         '/verify-a-cv': (context) => const VerifyACvRoute(),
-        '/my-company-administration': (context) => const MyCompanyAdministration(),
+        '/my-company-administration': (context) =>
+            const MyCompanyAdministration(),
         '/company/add-an-employee': (context) => const AddAnEmployee(),
-        '/educationInstitution/add-a-certificate-to-user': (context) => CreateACertificate(),
+        '/educationInstitution/add-a-certificate-to-user': (context) =>
+            CreateACertificate(),
       },
       onGenerateRoute: (settings) {
         if (settings.name != null && settings.name!.startsWith('/user/')) {
@@ -59,8 +64,10 @@ class MyApp extends StatelessWidget {
             builder: (context) => UserPage(id: id),
           );
         }
-        if (settings.name != null && settings.name!.startsWith('/check-my-skill-with-ai/')) {
-          final id = settings.name!.substring('/check-my-skill-with-ai/'.length);
+        if (settings.name != null &&
+            settings.name!.startsWith('/check-my-skill-with-ai/')) {
+          final id =
+              settings.name!.substring('/check-my-skill-with-ai/'.length);
           return MaterialPageRoute(
             builder: (context) => CheckMySkillsWithAIPage(id: id),
           );
@@ -79,6 +86,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-
