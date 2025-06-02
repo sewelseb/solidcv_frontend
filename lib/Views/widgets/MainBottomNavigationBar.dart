fbf3022ddc90fa824aa6bcc7b0d5cd6647d16f12
Fisnik Duraku <fisnik.duraku@student.vinci.be>
2025-06-02 02:48:34 +0200

    refactor: better design mainbottomNavBar

--- lib/Views/widgets/MainBottomNavigationBar.dart
+++ lib/Views/widgets/MainBottomNavigationBar.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
@@ -5,8 +6,7 @@ class MainBottomNavigationBar extends StatefulWidget {
   const MainBottomNavigationBar({super.key});
 
   @override
-  _MainBottomNavigationBarState createState() =>
-      _MainBottomNavigationBarState();
+  State<MainBottomNavigationBar> createState() => _MainBottomNavigationBarState();
 }
 
 class _MainBottomNavigationBarState extends State<MainBottomNavigationBar> {
@@ -34,48 +34,74 @@ class _MainBottomNavigationBarState extends State<MainBottomNavigationBar> {
 
   @override
   Widget build(BuildContext context) {
-    //select the right _selectedIndex based on the current route
-    if (ModalRoute.of(context)!.settings.name == '/loggedin/home') {
-      _selectedIndex = 0;
-    } else if (ModalRoute.of(context)!.settings.name == '/my-cv') {
-      _selectedIndex = 1;
-    } else if (ModalRoute.of(context)!.settings.name == '/my-organisation') {
-      _selectedIndex = 2;
-    } else if (ModalRoute.of(context)!.settings.name == '/verify-a-cv') {
-      _selectedIndex = 3;
-    }
+    final route = ModalRoute.of(context)?.settings.name;
+    if (route == '/loggedin/home') _selectedIndex = 0;
+    else if (route == '/my-cv') _selectedIndex = 1;
+    else if (route == '/my-organisation') _selectedIndex = 2;
+    else if (route == '/verify-a-cv') _selectedIndex = 3;
 
-    return BottomNavigationBar(
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home, color: Colors.grey),
-          activeIcon: Icon(Icons.home, color: Colors.amber),
-          label: 'Home',
+    final isMobile = MediaQuery.of(context).size.width < 600;
+
+    return ClipRRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+        child: Container(
+          width: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color(0xFF7B3FE4), Color(0xFFB57AED)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Color(0x337B3FE4),
+                blurRadius: 14,
+                offset: Offset(0, -3),
+              ),
+            ],
+          ),
+          padding: EdgeInsets.only(
+            top: isMobile ? 4 : 10,
+            bottom: MediaQuery.of(context).padding.bottom > 0 ? MediaQuery.of(context).padding.bottom : (isMobile ? 4 : 10),
+          ),
+          child: BottomNavigationBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.white70,
+            selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+            unselectedLabelStyle: const TextStyle(fontWeight: FontWeight.normal),
+            iconSize: isMobile ? 26 : 30,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.badge_outlined),
+                label: 'My CV',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.business_center_outlined),
+                label: 'Organisation',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.verified_outlined),
+                label: 'Verify CV',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.logout_outlined),
+                label: 'Logout',
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+            showUnselectedLabels: true,
+          ),
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.person, color: Colors.grey),
-          activeIcon: Icon(Icons.person, color: Colors.amber),
-          label: 'My CV',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.business, color: Colors.grey),
-          activeIcon: Icon(Icons.business, color: Colors.amber),
-          label: 'My Organisation',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.verified, color: Colors.grey),
-          activeIcon: Icon(Icons.verified, color: Colors.amber),
-          label: 'Verify a CV',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.logout, color: Colors.grey),
-          activeIcon: Icon(Icons.logout, color: Colors.amber),
-          label: 'Logout',
-        ),
-      ],
-      currentIndex: _selectedIndex,
-      selectedItemColor: Colors.amber[800],
-      onTap: _onItemTapped,
+      ),
     );
   }
 
@@ -101,7 +127,7 @@ class _MainBottomNavigationBarState extends State<MainBottomNavigationBar> {
     );
 
     if (confirm == true) {
-      final storage = FlutterSecureStorage();
+      const storage = FlutterSecureStorage();
       await storage.delete(key: 'jwt');
       Navigator.pushNamedAndRemoveUntil(context, '/login', (route) => false);
     }
