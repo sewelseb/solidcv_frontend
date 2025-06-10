3ac8bf86f9a05c6a0bdd97aed5fb0b08941c64f7
Fisnik Duraku <fisnik.duraku@student.vinci.be>
2025-06-10 14:52:40 +0200

    add: new model to combine certificates from blockchain and manually added

---
+++ lib/models/CertificatWrapper.dart
@@ -0,0 +1,7 @@
+import 'package:solid_cv/models/Certificate.dart';
+
+class CertificatWrapper {
+  final Certificate cert;
+  final bool isBlockchain;
+  CertificatWrapper(this.cert, this.isBlockchain);
+}
