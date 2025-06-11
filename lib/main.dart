9cffa96e77fe42636b7bb89fcceb726272fef245
Fisnik Duraku <fisnik.duraku@student.vinci.be>
2025-06-11 16:35:18 +0200

    add: route for reset password and verify email

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,9 @@ import 'package:solid_cv/Views/admin-views/AdminUserListPage.dart';
 import 'package:solid_cv/Views/companyViews/AddAnEmployee.dart';
 import 'package:solid_cv/Views/educationInstitutionViews/CreateACertificate.dart';
 import 'package:solid_cv/Views/widgets/AuthGuard.dart';
-import 'package:solid_cv/Views/widgets/EmailWidgets/EmailSuccessedPage.dart';
-import 'package:solid_cv/Views/widgets/EmailWidgets/EmailVerifyFailedPage.dart';
+import 'package:solid_cv/Views/widgets/EmailWidgets/EmailVerificationResultPage.dart';
+import 'package:solid_cv/Views/widgets/EmailWidgets/ForgotPasswordPage.dart';
+import 'package:solid_cv/Views/widgets/EmailWidgets/ResetPasswordPage.dart';
 import 'package:solid_cv/Views/widgets/EmailWidgets/VerifyEmailPage.dart';
 import 'package:solid_cv/Views/widgets/userWidgets/EditUserProfile.dart';
 import 'package:solid_cv/models/User.dart';
@@ -50,48 +51,76 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => const HomeRoute(),
         '/register': (context) => const RegisterRoute(),
-        '/loggedin/home': (context) => const AuthGuard(child:LoggedInHome()),
-        '/my-cv': (context) => const AuthGuard(child:MyCvRoute()),
-        '/my-organisation': (context) => const AuthGuard(child:MyOrganisationsRoute()),
+        '/loggedin/home': (context) => const AuthGuard(child: LoggedInHome()),
+        '/my-cv': (context) => const AuthGuard(child: MyCvRoute()),
+        '/my-organisation': (context) =>
+            const AuthGuard(child: MyOrganisationsRoute()),
         '/my-educationInstitution-administration': (context) =>
-            const AuthGuard(child:MyEducationInstitutionAdministration()),
+            const AuthGuard(child: MyEducationInstitutionAdministration()),
         '/user/edit-profile': (context) {
           final user = ModalRoute.of(context)?.settings.arguments as User;
-          return AuthGuard(child:EditProfileRoute(user: user));
+          return AuthGuard(child: EditProfileRoute(user: user));
         },
-        '/verify-email': (context) {
+        '/forgot-password': (context) => const ForgotPasswordPage(),
+        '/reset-password': (context) {
+          final uri = Uri.base;
+          final token = uri.queryParameters['token'];
+          return ResetPasswordPage(token: token ?? '');
+        },
+        '/sent-verification-email': (context) {
           final email = ModalRoute.of(context)?.settings.arguments as String?;
           return VerifyEmailPage(email: email ?? '');
         },
-        '/email-verified-success': (context) => const EmailVerifiedSuccessPage(),
-        '/email-verify-failed': (context) => const EmailVerifyFailedPage(),
-        '/add-a-company-form': (context) => const AuthGuard(child:AddACompanyFormRoute()),
+        '/add-a-company-form': (context) =>
+            const AuthGuard(child: AddACompanyFormRoute()),
         '/add-a-education-institution-form': (context) =>
-            const AuthGuard(child:AddanEducationInstitutionFormRoute()),
-        '/verify-a-cv': (context) => const AuthGuard(child:VerifyACvRoute()),
+            const AuthGuard(child: AddanEducationInstitutionFormRoute()),
+        '/verify-a-cv': (context) => const AuthGuard(child: VerifyACvRoute()),
         '/my-company-administration': (context) =>
-            const AuthGuard(child:MyCompanyAdministration()),
-        '/company/add-an-employee': (context) => const AuthGuard(child:AddAnEmployee()),
+            const AuthGuard(child: MyCompanyAdministration()),
+        '/company/add-an-employee': (context) =>
+            const AuthGuard(child: AddAnEmployee()),
         '/educationInstitution/add-a-certificate-to-user': (context) =>
-            AuthGuard(child:CreateACertificate()),
-        '/admin/dashboard': (context) => const AuthGuard(child:AdminDashboardPage()),
-        '/admin/users': (context) => AuthGuard(child:AdminUsersPage()),
-        '/admin/companies': (context) => const AuthGuard(child:AdminCompaniesPage()),
-        '/admin/institutions': (context) => const AuthGuard(child:AdminInstitutionsPage()),
+            AuthGuard(child: CreateACertificate()),
+        '/admin/dashboard': (context) =>
+            const AuthGuard(child: AdminDashboardPage()),
+        '/admin/users': (context) => AuthGuard(child: AdminUsersPage()),
+        '/admin/companies': (context) =>
+            const AuthGuard(child: AdminCompaniesPage()),
+        '/admin/institutions': (context) =>
+            const AuthGuard(child: AdminInstitutionsPage()),
       },
       onGenerateRoute: (settings) {
         if (settings.name != null && settings.name!.startsWith('/user/')) {
           final id = settings.name!.substring('/user/'.length);
           return MaterialPageRoute(
-            builder: (context) => AuthGuard(child:UserPage(id: id)),
+            builder: (context) => AuthGuard(child: UserPage(id: id)),
           );
         }
+
+        if (settings.name != null &&
+            settings.name!.startsWith('/verify-email/')) {
+          final token = settings.name!.substring('/verify-email/'.length);
+          return MaterialPageRoute(
+            builder: (context) => EmailVerificationResultPage(token: token),
+          );
+        }
+
+        if (settings.name != null &&
+            settings.name!.startsWith('/reset-password/')) {
+          final token = settings.name!.substring('/reset-password/'.length);
+          return MaterialPageRoute(
+            builder: (context) => ResetPasswordPage(token: token),
+          );
+        }
+
         if (settings.name != null &&
             settings.name!.startsWith('/check-my-skill-with-ai/')) {
           final id =
               settings.name!.substring('/check-my-skill-with-ai/'.length);
           return MaterialPageRoute(
-            builder: (context) => AuthGuard(child:CheckMySkillsWithAIPage(id: id)),
+            builder: (context) =>
+                AuthGuard(child: CheckMySkillsWithAIPage(id: id)),
           );
         }
         return null; // Let `onUnknownRoute` handle this case.
