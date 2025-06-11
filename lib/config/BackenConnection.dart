706f29bfede566b85069f871939a7d3e70878fd6
Fisnik Duraku <fisnik.duraku@student.vinci.be>
2025-06-11 13:39:16 +0200

    add: resendEmail backend route

--- lib/config/BackenConnection.dart
+++ lib/config/BackenConnection.dart
@@ -88,4 +88,6 @@ class BackenConnection {
   String get updateEducationInstitution=> "/api/protected/update-education-institution/";
 
   String get verifyEmail => "/api/verify-email";
+
+  String get resendEmailVerification => "/api/resend-email-verification";
 }
