3c69247036895eaf9283219732fcdfc16a9cf3a3
Fisnik Duraku <fisnik.duraku@student.vinci.be>
2025-06-13 15:44:43 +0200

    add: enter key to sign in in Login

--- lib/Views/HomeRoute.dart
+++ lib/Views/HomeRoute.dart
@@ -17,6 +17,17 @@ class _HomeRouteState extends State<HomeRoute> {
   final IUserBLL _userBll = UserBll();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final FocusNode _emailFocusNode = FocusNode();
+  final FocusNode _passwordFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _emailFocusNode.dispose();
+    _passwordFocusNode.dispose();
+    super.dispose();
+  }
 
   Future<void> _handleLogin() async {
     final email = _emailController.text.trim();
@@ -115,6 +126,8 @@ class _HomeRouteState extends State<HomeRoute> {
               },
               emailController: _emailController,
               passwordController: _passwordController,
+              emailFocusNode: _emailFocusNode,
+              passwordFocusNode: _passwordFocusNode,
               onLoginPressed: _handleLogin,
             ),
           ),
@@ -147,6 +160,8 @@ class _HomeRouteState extends State<HomeRoute> {
                   },
                   emailController: _emailController,
                   passwordController: _passwordController,
+                  emailFocusNode: _emailFocusNode,
+                  passwordFocusNode: _passwordFocusNode,
                   onLoginPressed: _handleLogin,
                 ),
               ),
@@ -179,6 +194,8 @@ class _LoginForm extends StatelessWidget {
   final TextEditingController emailController;
   final TextEditingController passwordController;
   final VoidCallback onLoginPressed;
+  final FocusNode emailFocusNode;
+  final FocusNode passwordFocusNode;
 
   const _LoginForm({
     required this.isMobile,
@@ -187,6 +204,8 @@ class _LoginForm extends StatelessWidget {
     required this.emailController,
     required this.passwordController,
     required this.onLoginPressed,
+    required this.emailFocusNode,
+    required this.passwordFocusNode,
   });
 
   @override
@@ -217,6 +236,9 @@ class _LoginForm extends StatelessWidget {
         const SizedBox(height: 32),
         TextField(
           controller: emailController,
+          focusNode: emailFocusNode,
+          onSubmitted: (_) =>
+              FocusScope.of(context).requestFocus(passwordFocusNode),
           decoration: InputDecoration(
             labelText: "Email address",
             hintText: "example@domain.com",
@@ -232,6 +254,8 @@ class _LoginForm extends StatelessWidget {
         const SizedBox(height: 16),
         TextField(
           controller: passwordController,
+          focusNode: passwordFocusNode,
+          onSubmitted: (_) => onLoginPressed(),
           obscureText: obscurePassword,
           decoration: InputDecoration(
             labelText: "Password",
